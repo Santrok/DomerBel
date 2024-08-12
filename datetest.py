3f9f28c995e67bed2bb56496e313576a202eac11
Alexander <a.povedaiko@yandex.ru>
2024-08-12 17:22:57 +0300

    second commit

--- datetest.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-#
-# print((datetime.now() - timedelta(days=50)) > (datetime.now() - timedelta(days=51)))
-
-
-x= datetime.now()
-y = x + timedelta(1)
-
-print(y<x)
