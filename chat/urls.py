754c4350df543d6bd7c0a8f93cb46c7d34e47812
Alexander <a.povedaiko@yandex.ru>
2024-08-18 17:16:26 +0300

    style dialog

--- chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from chat.views import chat_page
-
-urlpatterns = [
-    path("", chat_page, name="chat-page"),
-
-]
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,1 @@
 from django.shortcuts import render, redirect
-
-
-# Create your views here.
-
-
-def chat_page(request, *args, **kwargs):
-    if not request.user.is_authenticated:
-        return redirect("home")
-    context = {}
-    return render(request, "chat.html", context)
