5d4db1adf3730253cc70f09b6b75a8b32d224e72
Kostofei <dobysh@gmail.com>
2024-08-27 18:06:31 +0300

    перенес конвертацию в отдельную функцию

--- advertisement/utils_for_models.py
+++ advertisement/utils_for_models.py
@@ -1,11 +1,14 @@
 import random
 import os
+import pillow_avif
+from io import BytesIO
 from datetime import date
 
 from uuid import uuid4
 from slugify import slugify
 from PIL import Image, ImageDraw, ImageFont
 from hashlib import md5
+from django.core.files.base import ContentFile
 
 # from .models import Advertisement
 
@@ -53,3 +56,24 @@ def unique_slugify(instance, slug):
     while model.objects.filter(slug=unique_slug).exists():
         unique_slug = f'{unique_slug}-{uuid4().hex[:8]}'
     return unique_slug
+
+
+def convert_image_to_avif(photo):
+
+    # Открываем загруженный файл с помощью Pillow
+    img = Image.open(photo)
+
+    # Создаём временный буфер для сохранения изображения в формате AVIF
+    img_io = BytesIO()
+
+    # Сохраняем изображение в формате AVIF
+    img.save(img_io, format='AVIF')
+
+    # Перематываем буфер обратно в начало
+    img_io.seek(0)
+
+    # Генерируем новое имя файла с расширением .avif
+    new_filename = os.path.splitext(photo.name)[0] + '.avif'
+
+    # Обновляем файл в поле photo с новым расширением
+    photo.save(new_filename, ContentFile(img_io.read()), save=False)
