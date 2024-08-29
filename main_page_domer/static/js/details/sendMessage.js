edd3a3db50fc2acffd344500a6be8680450c1817
Artsem Yunchyts <artem131103@yandex.ru>
2024-08-29 18:45:49 +0300

    test reset modal

--- main_page_domer/static/js/details/sendMessage.js
+++ main_page_domer/static/js/details/sendMessage.js
@@ -1,36 +1,50 @@
-const sendMessageBtn = [...document.querySelectorAll(".details__advertisement-send-message"), ...document.querySelectorAll(".store__send-email")]
+const sendMessageBtn = [
+  ...document.querySelectorAll(".details__advertisement-send-message"),
+  ...document.querySelectorAll(".store__send-email"),
+]
 const blockModals = document.querySelector(".modals")
 const sendMessageModal = document.querySelector(".send__message")
 const sendMessageFormBtn = document.querySelector(".send__message button")
 
-sendMessageBtn.forEach(item => item.addEventListener("click", () => {
-  if(item.dataset.id) {
-    blockModals.classList.add("modal__active")
-    sendMessageModal.classList.add("modal__active")
-  } else {
-    modal.classList.add("modal__active")
-    modalLogin.classList.add("modal__active")
-  }
-}) 
+sendMessageBtn.forEach((item) =>
+  item.addEventListener("click", () => {
+    if (item.dataset.id) {
+      blockModals.classList.add("modal__active")
+      sendMessageModal.classList.add("modal__active")
+    } else {
+      modal.classList.add("modal__active")
+      modalLogin.classList.add("modal__active")
+    }
+  })
 )
 
 sendMessageFormBtn.addEventListener("click", sendMessage)
 
 function sendMessage() {
   const data = new FormData(sendMessageModal)
-  data.append("advertisement", sendMessageBtn.find(item => item.dataset.id).dataset.id)
-  fetch(`${window.location.protocol}//${window.location.host}/api/v1/create_chat/`, {
-    method: "POST",
-    headers: {
-      "X-CSRFToken": getCookie("csrftoken"),
-    },
-    body: data
-  })
-      .then(resp=> resp.json())
-      .then(data => {
-        document.querySelector('.modals__notification').classList.add('modal__active')
-        modal.classList.add('modal__active')
-        document.querySelector('.modals__notification-text').innerText = data.success
-          sendMessageModal.classList.remove('modal__active')
-      })
+  data.append(
+    "advertisement",
+    sendMessageBtn.find((item) => item.dataset.id).dataset.id
+  )
+  fetch(
+    `${window.location.protocol}//${window.location.host}/api/v1/create_chat/`,
+    {
+      method: "POST",
+      headers: {
+        "X-CSRFToken": getCookie("csrftoken"),
+      },
+      body: data,
+    }
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      document
+        .querySelector(".modals__notification")
+        .classList.add("modal__active")
+      modal.classList.add("modal__active")
+      document.querySelector(".modals__notification-text").innerText =
+        data.success
+      sendMessageModal.classList.remove("modal__active")
+    })
+  sendMessageModal.reset()
 }
