6d3b29c8c92c5e4afc1e93ba543effe4e48cbb89
Kostofei <dobysh@gmail.com>
2024-09-08 06:51:45 +0300

    скрипт готов

--- edit_db.py
+++ edit_db.py
@@ -10,7 +10,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 # Инициализируем Django
 django.setup()
 
-from advertisement.models import Advertisement, PhotoAdvertisement
+from advertisement.models import Advertisement, PhotoAdvertisement, Store
 
 advertisements = Advertisement.objects.filter(preview_image__endswith='.jpg')
 print('Всего объявлений:', advertisements.count())
@@ -22,4 +22,10 @@ photo_advertisements = PhotoAdvertisement.objects.filter(photo__endswith='.jpg')
 print('Всего фото объявлений:', photo_advertisements.count())
 data_start = datetime.now()
 photo_advertisements.update(photo=Replace(F('photo'), Value('.jpg'), Value('.avif')))
+print(datetime.now() - data_start)
+
+stores = Store.objects.filter(logo_image__endswith='.jpg')
+print('Всего фото объявлений:', stores.count())
+data_start = datetime.now()
+stores.update(logo_image=Replace(F('logo_image'), Value('.jpg'), Value('.avif')))
 print(datetime.now() - data_start)
