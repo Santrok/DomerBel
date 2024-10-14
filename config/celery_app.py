23b7ab41ede4fd388ea8e91d140a2a82dc8ffafa
Kostofei <dobysh@gmail.com>
2024-10-14 12:24:39 +0300

    доделана задача update_schedule v1.3

--- config/celery_app.py
+++ config/celery_app.py
@@ -19,30 +19,30 @@ app.conf.result_expires = 15
 
 '''плановые задачи'''
 app.conf.beat_schedule = {
-    # "deactivate_advertisement": {
-    #     "task": 'advertisement.tasks.deactivate_advertisement',
-    #     # "schedule": timedelta(seconds=10)
-    #     "schedule": crontab(hour=0, minute=1)
-    # },
-    # "delete_advertisement": {
-    #     "task": 'advertisement.tasks.delete_advertisement',
-    #     # "schedule": timedelta(seconds=10)
-    #     "schedule": crontab(hour=0, minute=1)
-    # },
-    # "deactivate_store": {
-    #     "task": 'advertisement.tasks.deactivate_store',
-    #     # "schedule": timedelta(seconds=10)
-    #     "schedule": crontab(hour=0, minute=1)
-    # },
-    # "delete_everything_in_folder": {
-    #     "task": "advertisement.tasks.delete_everything_in_folder_beat",
-    #     "schedule": crontab(minute=15, hour=0),
-    # },
-    # "delete_error_file": {
-    #     "task": "advertisement.tasks.delete_error_file_beat",
-    #     "schedule": crontab(minute=15, hour=0),
-    #     # "schedule": timedelta(seconds=120)
-    # },
+    "deactivate_advertisement": {
+        "task": 'advertisement.tasks.deactivate_advertisement',
+        # "schedule": timedelta(seconds=10)
+        "schedule": crontab(hour=0, minute=1)
+    },
+    "delete_advertisement": {
+        "task": 'advertisement.tasks.delete_advertisement',
+        # "schedule": timedelta(seconds=10)
+        "schedule": crontab(hour=0, minute=1)
+    },
+    "deactivate_store": {
+        "task": 'advertisement.tasks.deactivate_store',
+        # "schedule": timedelta(seconds=10)
+        "schedule": crontab(hour=0, minute=1)
+    },
+    "delete_everything_in_folder": {
+        "task": "advertisement.tasks.delete_everything_in_folder_beat",
+        "schedule": crontab(minute=15, hour=0),
+    },
+    "delete_error_file": {
+        "task": "advertisement.tasks.delete_error_file_beat",
+        "schedule": crontab(minute=15, hour=0),
+        # "schedule": timedelta(seconds=120)
+    },
     "list_shown_vip": {
         "task": "advertisement.tasks.list_shown_vip",
         "schedule": timedelta(seconds=15)
