986b3980164cf6b265862378c4de7e4346f8a355
Alexander <a.povedaiko@yandex.ru>
2024-10-17 16:31:30 +0300

    исправлен способ регистрации юридического лица

--- custom_user/signals.py
+++ custom_user/signals.py
@@ -11,19 +11,19 @@ from store.models import Store
 from .models import UserFavorites
 
 
-# @receiver(post_save, sender=get_user_model())
-# def add_user_in_permission_group(sender, instance, **kwargs):
-#     """
-#     Добавление пользователя в группу с правами доступными только юр. лицам
-#     """
-#     group = Group.objects.get_or_create(name='Юридические лица')
-#     try:
-#         if instance.entity:
-#             transaction.on_commit(lambda: instance.groups.add(group[0]))
-#         else:
-#             transaction.on_commit(lambda: instance.groups.remove(group[0]))
-#     except Exception:
-#         pass
+@receiver(post_save, sender=get_user_model())
+def add_user_in_permission_group(sender, instance, **kwargs):
+    """
+    Добавление пользователя в группу с правами доступными только юр. лицам
+    """
+    group = Group.objects.get_or_create(name='Юридические лица')
+    try:
+        if instance.entity:
+            transaction.on_commit(lambda: instance.groups.add(group[0]))
+        else:
+            transaction.on_commit(lambda: instance.groups.remove(group[0]))
+    except Exception:
+        pass
 
 
 @receiver(post_save, sender=get_user_model())
