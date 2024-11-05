22f1854bf0c4e537d077b9058b943dee652eb900
Alexander <a.povedaiko@yandex.ru>
2024-11-05 18:54:19 +0300

    sitemap

--- advertisement/models.py
+++ advertisement/models.py
@@ -5,6 +5,7 @@ import PIL
 from dirtyfields import DirtyFieldsMixin
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.sitemaps import Sitemap
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.postgres.indexes import GinIndex, OpClass, BrinIndex
 from django.contrib.postgres.search import SearchVectorField
@@ -259,9 +260,6 @@ class AdvertisementAdmin(admin.ModelAdmin):
     ordering = ['-date_of_last_activation']
     list_per_page = 50
     inlines = [PhotoAdvertisementInlines]
-
-
-from django.contrib.sitemaps import Sitemap
 
 
 class AdvertisementSitemap(Sitemap):
